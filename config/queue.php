53d97a4b71020d2dfc81a093f13d481b27b4351e
CC <664985936@qq.com>
2016-01-12 15:41:26 +0800

    Update queue.php

--- config/queue.php
+++ config/queue.php
@@ -23,19 +23,19 @@ return [
         [
             'tube' => 'testjob1',//队列的名称
             'job'  => 'src\Web\Queue\TestJob',//需要执行的任务
-            //以下参数选填 不填默认读取外层的配置
+            //处理当前队列的进程数
             'task_worker_num' => 3,
         ],
         [
             'tube' => 'testjob3',//队列的名称
             'job'  => 'src\Web\Queue\TestJob',//需要执行的任务
-            //以下参数选填 不填默认读取外层的配置
+            //处理当前队列的进程数
             'task_worker_num' => 1,
         ],
         [
             'tube' => 'testjob2',//队列的名称
             'job'  => 'src\Web\Queue\TestJob',//需要执行的任务
-            //以下参数选填 不填默认读取外层的配置
+            //处理当前队列的进程数
             'task_worker_num' => 2,
         ]
 
