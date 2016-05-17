9157847f146d43d6c0a52386fb05b78546a08929
fucongcong <664985936@qq.com>
2016-05-17 18:03:19 +0800

    add

--- asset/js/list.js
+++ asset/js/list.js
@@ -7,7 +7,7 @@ define('/asset/js/list', function(require, exports, module) {
     })
 
     var pageCount = 1;
-    var dragger = new DragLoader(document.getElementsByClassName('list_body')[0], {
+    var dragger = new DragLoader(document.getElementsByClassName('body')[0], {
             dragDownRegionCls: 'latest',
             dragUpRegionCls: 'more',
             // dragDownHelper: function(status) {console.log(1);
@@ -29,7 +29,7 @@ define('/asset/js/list', function(require, exports, module) {
                 }
             }
         });
-        dragger.on('dragDownLoad', function() {
+        dragger.on('dragDownLoad', function() {console.log(1)
             setTimeout(function() {
                 // 无论何时，必须由业务功能主动调用reset()接口，以还原状态
                 // 比如在onDragDownLoad()回调中使用ajax加载数据时，在ajax的回调函数中应当调用reset()重置drag状态
@@ -37,40 +37,13 @@ define('/asset/js/list', function(require, exports, module) {
                 dragger.reset();
             }, 200);
         });
-        dragger.on('dragUpLoad', function() {
+        dragger.on('dragUpLoad', function() {console.log(2)
             key = $('#list_body').data('key');
             $.get('/group/list/more', {start:pageCount * 10, key:key}, function(res){
                 $('.ul-list').append(res);
                 pageCount++;
                 dragger.reset();
             })
+            dragger.reset();
         });
-
-
-    // var loadingCount = 0,
-    //     totalPages = $('.list_body').data('totalPage');
-    // var pageCount;
-    // using jQuery plugin instantiation
-    // $('#list_body').infiniteScrollHelper({
-        
-    //     loadMore: function(page, done) {
-    //         $('#list_body').append('asdsad<div style=\'height:800px;\'>asd<div>'); done();
-    //         // ajax request would be kicked off here
-    //         pageCount = page;
-    //         //$('.loader').text('Loading Page ' + page + '...');
-    //         console.log(1);
-            // simulating loading of some content
-            
-            
-    //         // if (pageCount == totalPages) { // if we are at the last page, destroy the plugin instance
-    //         //     console.log('desroying');
-    //         //     $('#list_body').infiniteScrollHelper('destroy');
-    //         // }
-
-    //         // done();
-    //     },
-    //     //bottomBuffer: 1200,
-    //     // using the triggerInitialLoad option
-    //     //triggerInitialLoad: true
-    // })
 });
