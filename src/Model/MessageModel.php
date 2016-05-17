a63ecbaca98c6c1f89782674b94737efe73c97aa
fucongcong <664985936@qq.com>
2016-05-17 11:26:18 +0800

    fix

--- src/Model/MessageModel.php
+++ src/Model/MessageModel.php
@@ -16,7 +16,7 @@ class MessageModel extends Model {
 
     public function listMessage($uid, $to_uid)
     {   
-        $this -> data(['is_read' => 1]) -> where(['to_uid' => $to_uid, 'uid' => $uid]) -> save();
+        $this -> data(['is_read' => 1]) -> where(['to_uid' => $uid]) -> save();
         $sql = "SELECT * FROM message WHERE (uid={$uid} AND to_uid={$to_uid}) OR (uid={$to_uid} AND to_uid={$uid}) ORDER BY ctime DESC";
         return M() -> query($sql);
     }
