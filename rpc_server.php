aea3af03f54e50d12e0093663c90ed86935cd81e
fucongcong <fucongcong@clothesmake.com>
2016-12-26 16:29:28 +0800

    修复rpc服务依赖库升级不可用的问题

--- rpc_server.php
+++ rpc_server.php
@@ -1,8 +1,9 @@
 <?php
 
 define('__ROOT__', realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR);
-require_once __ROOT__.'vendor/hprose/hprose/src/Hprose.php';
+require_once __ROOT__.'vendor/group/group-framework/core/Group/Plugin/Rpc/Hprose.php';
 require_once __ROOT__.'vendor/group/group-framework/core/Group/RpcKernal.php';
 
+new \Group\Plugin\Rpc\Hprose();
 $kernal = new RpcKernal('tcp');
 $kernal->init();
