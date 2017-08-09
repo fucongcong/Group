0bc7336c7f0211c6a7102094a8f56959222af2e5
fucongcong <fucongcong@clothesmake.com>
2017-08-09 09:32:22 +0800

    fix the wanring

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 $loader = require __DIR__.'/vendor/autoload.php';
 $loader->setUseIncludePath(true);
 
+define('SWOOLE_HTTP', false);
+
 $kernal = new \Group\Kernal();
 $kernal->init(realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR, $loader);
 
